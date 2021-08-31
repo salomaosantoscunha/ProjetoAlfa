bc94ba9f8fba43c43abeebeed54272cf71f869ad
Salomão <sal-07@hotmail.com>
2021-08-31 09:29:52 -0300

    actualizations

---
+++ src/Calculate.java
@@ -0,0 +1,29 @@
+public class Calculate {
+    public static void main(String[] args) {
+        int x = Integer.parseInt(args[1]);
+        int y = Integer.parseInt(args[2]);
+        if (args[0].equals("soma")) {
+            sum(x, y);
+        } else if (args[0].equals("subtrair")) {
+            minus(x, y);
+        }
+        else if (args[0].equals("multiplicar")) {
+            mult(x, y);
+        } else if (args[0].equals("dividir")) {
+            div(x, y);
+        }else {
+            System.out.println("invalido");
+        }
+
+    }
+
+    static void sum(int x, int y) {
+        System.out.println(x + y);
+    }
+    static void minus(int x, int y) {
+        System.out.println(x - y);
+    }  static void mult(int x, int y) {
+        System.out.println(x * y);
+}  static void div(int x, int y) {
+        System.out.println(x / y);
+    }}
---
+++ src/ProjetoAlfa.java
@@ -0,0 +1,7 @@
+public class ProjetoAlfa {
+
+    public static void main(String[] args) {
+
+ System.out.println("Ola," + args[0]);
+ System.out.println("Ola," + args[1]);   }
+}
